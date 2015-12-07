8e65ffb73ce77db48191cbf9987509464f24436e
rfcampbell <mantid@gmail.com>
2015-12-07 11:06:46 -0800

    Update am-i-robot.js

--- scripts/am-i-robot.js
+++ scripts/am-i-robot.js
@@ -11,7 +11,7 @@
 //   hubot are you a robot?  - returns whether is it weekend or not
 
 module.exports = function(robot) {
-    robot.respond(/are you a robot\s?\?/i, function(msg){
-        msg.reply("I don't think therefore I am");
+    robot.respond(/do you like dogs\s?\?/i, function(msg){
+        msg.reply("yes");
     });
 }
